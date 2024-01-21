f771bc771edd5cf4e7ec59d917345e72706f424b
Nicholas Breitling <breitling.nw@gmail.com>
2024-01-21 11:53:10 -0500

    snoc 3 :)

--- HW2/src/ExamplesLoString.java
+++ HW2/src/ExamplesLoString.java
@@ -32,26 +32,26 @@ public class ExamplesLoString {
     ILoString rEverybody = new SnocLoString(rDwarves, "Snow White");
     
     return t.checkExpect(addBashful.reverse().normalize(), rAddBashful.normalize())
-      && t.checkExpect(everybody.reverse().normalize(), rEverybody.normalize());
+             && t.checkExpect(everybody.reverse().normalize(), rEverybody.normalize());
   }
   
   boolean testNormalize(Tester t) {
     ILoString nEverybody = new ConsLoString("Snow White",
-        new ConsLoString("Bashful",
-            new ConsLoString("Doc",
-                new ConsLoString("Dopey",
-                    new ConsLoString("Grumpy",
-                        new ConsLoString("Happy",
-                            new ConsLoString("Sleepy",
-                                new ConsLoString("Sneezy",
-                                    empty
-                                )
-                            )
-                        )
-                    )
+      new ConsLoString("Bashful",
+        new ConsLoString("Doc",
+          new ConsLoString("Dopey",
+            new ConsLoString("Grumpy",
+              new ConsLoString("Happy",
+                new ConsLoString("Sleepy",
+                  new ConsLoString("Sneezy",
+                    empty
+                  )
                 )
+              )
             )
+          )
         )
+      )
     );
     
     return t.checkExpect(everybody.normalize(), nEverybody);
@@ -62,18 +62,21 @@ public class ExamplesLoString {
     ILoString sAddDoc = new SnocLoString(empty, "Snow WhiteBashfulDoc");
     ILoString sAddBashful = new ConsLoString("Snow WhiteBashful", sAddDoc);
     ILoString sAddDopey = new SnocLoString(sAddBashful, "Snow WhiteBashfulDocDopey");
-
+    
     ILoString sGrumpy = new ConsLoString("Snow WhiteBashfulDocDopeyGrumpy", empty);
-
+    
     ILoString sAddSleepy = new ConsLoString("Snow WhiteBashfulDocDopeyGrumpyHappySleepy", empty);
     ILoString sAddHappy = new ConsLoString("Snow WhiteBashfulDocDopeyGrumpyHappy", sAddSleepy);
     ILoString sAddSneezy = new SnocLoString(sAddHappy, "Snow WhiteBashfulDocDopeyGrumpyHappySleepySneezy");
-
+    
     ILoString sDwarves = new AppendLoString(new AppendLoString(sAddDopey, sGrumpy), sAddSneezy);
     
     ILoString sEverybody = new ConsLoString("Snow White", sDwarves);
-
+    
+    ILoString oneList = new SnocLoString(new SnocLoString(empty, "one"), "two");
+    
     return t.checkExpect(everybody.scanConcat(), sEverybody.normalize())
-      && t.checkExpect(empty.scanConcat(), empty);
+             && t.checkExpect(empty.scanConcat(), empty)
+             && t.checkExpect(oneList.scanConcat(), new ConsLoString("one", new ConsLoString("onetwo", empty)));
   }
 }
