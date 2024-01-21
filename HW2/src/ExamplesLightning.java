7f7269ad172bc11e91b2f1e3b3a854c0a0c31937
Harsh31105 <harshnirajsingh@gmail.com>
2024-01-21 16:00:13 -0500

    Normalize Jaun Dice

--- HW2/src/ExamplesLightning.java
+++ HW2/src/ExamplesLightning.java
@@ -1,2 +1,44 @@
+import lightning.*;
+import tester.*;                // The tester library
+import javalib.worldimages.*;   // images, like RectangleImage or OverlayImages
+import javalib.funworld.*;      // the abstract World class and the big-bang library
+import java.awt.Color;          // general colors (as triples of red,green,blue values)
+// and predefined colors (Color.RED, Color.GRAY, etc.)
+
 public class ExamplesLightning {
+  ILightningBolt tip = new Tip();
+  ILightningBolt bolt1 = new Fork(33, 30, 10, 25, 135, 40, new Tip(), new Tip());
+  ILightningBolt bolt2 = new Fork(30, 22, 30, 10, 115, 65, new Tip(), new Tip());
+  ILightningBolt seg1 = new Segment(40, 20, 130, bolt1); // not plausible!
+  ILightningBolt seg2 = new Segment(50, 30, 77, bolt2); // plausible!
+
+  ILightningBolt forked = new Fork(40, 50, 50, 40, 150, 30, seg1, seg2);
+
+  boolean testIsPhysicallyPossible(Tester t) {
+    return t.checkExpect(seg1.isPhysicallyPossible(), false)
+            && t.checkExpect(seg2.isPhysicallyPossible(), true)
+            && t.checkExpect(forked.isPhysicallyPossible(), false);
+            // && t.checkExpect(bolt1.isPhysicallyPosibble(), true)
+            // && t.checkExpect(tip.isPhysicallyPossible(), true);
+  }
+
+  boolean testCombine(Tester t) {
+    int lAngleOffset = 150 - 90;
+    int rAngleOffset = 30 - 90;
+
+    ILightningBolt combined = seg1.combine(40, 50, 50, 40, 150, 30, seg2);
+
+    return t.checkExpect(combined,
+            new Fork(40, 50, 50, 40, 150, 30,
+                    new Segment(40, 20, 130 + lAngleOffset,
+                            new Fork(33, 30, 10, 25, 135 + lAngleOffset, 40 + lAngleOffset, new Tip(), new Tip())),
+                    new Segment(50, 30, 77 + rAngleOffset,
+                            new Fork(30, 22, 30, 10, 115 + rAngleOffset, 65 + rAngleOffset, new Tip(), new Tip()))));
+  }
+
+//  boolean testGetWidth(Tester t) {
+//    return t.checkExpect(seg1.getWidth(), )
+//  }
+
 }
+
--- HW2/src/MoreLists.java
+++ HW2/src/MoreLists.java
@@ -1,2 +1,69 @@
-public class MoreLists {
+interface ILoString {
+  // ILoString reverse();
+  ILoString normalize();
+  // ILoString scanConcat();
 }
+
+class ConsLoString implements ILoString {
+  String first;
+  ILoString rest;
+
+  ConsLoString(String first, ILoString rest) {
+    this.first = first;
+    this.rest = rest;
+  }
+
+  // public ILoString reverse() {}
+
+  public ILoString normalize() {
+    return new ConsLoString(this.first, this.rest.normalize());
+  }
+
+  // public ILoString scanConcat() {}
+}
+
+class MtLoString implements ILoString {
+  // public ILoString reverse() {}
+
+  public ILoString normalize() {
+    return this;
+  }
+
+  // public ILoString scanConcat() {}
+}
+
+class SnocLoString implements ILoString {
+  ILoString rest;
+  String last;
+
+  SnocLoString(ILoString rest, String last) {
+    this.rest = rest;
+    this.last = last;
+  }
+  // public ILoString reverse() {}
+
+  public ILoString normalize() {
+    return new AppendLoString(this.rest.normalize(), new ConsLoString(this.last, new MtLoString()));
+  }
+
+  // public ILoString scanConcat() {}
+}
+
+class AppendLoString implements ILoString {
+  ILoString list1;
+  ILoString list2;
+
+  AppendLoString(ILoString list1, ILoString list2) {
+    this.list1 = list1;
+    this.list2 = list2;
+  }
+  // public ILoString reverse() {}
+
+  public ILoString normalize() {
+    return new AppendLoString(this.list1.normalize(), this.list2.normalize());
+  }
+
+  // public ILoString scanConcat() {}
+}
+
+// Given Auto-generated Code: public class MoreLists { }
