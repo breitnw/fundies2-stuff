f0f05cec76a85af2e4367b795e3654e9eea49e7b
Nicholas Breitling <breitling.nw@gmail.com>
2024-01-23 16:01:58 -0500

    Almost finished with HW1

--- HW2/src/Lightning.java
+++ HW2/src/Lightning.java
@@ -45,6 +45,22 @@ interface ILightningBolt {
 
 // represents one final endpoint of a lightning bolt
 class Tip implements ILightningBolt {
+  /* TEMPLATE:
+  METHODS:
+  ... draw() ...                                      -- WorldImage
+  ... isPhysicallyPossible()                          -- boolean
+  ... isPhysicallyPossibleHelper(int parentCurrent)   -- boolean
+  ... combine(int leftLength, int rightLength,
+              int leftCapacity, int rightCapacity,
+              double leftTheta, double rightTheta,
+              ILightningBolt otherBolt) ...           -- ILightningBolt
+  ... offsetAll(double theta) ...                     -- ILightningBolt
+  ... getWidth() ...                                  -- double
+  ... getRightmostX() ...                             -- double
+  ... getLeftmostX() ...                              -- double
+   */
+  
+  // Returns an orange circle to represent this tip in an image.
   public WorldImage draw() {
     return new CircleImage(5, OutlineMode.SOLID, Color.ORANGE);
   }
@@ -58,6 +74,10 @@ class Tip implements ILightningBolt {
   // Returns true iff this Tip is physically possible based on the current of its parent. A tip is
   // always physically possible regardless of the parent current, so true is always returned.
   public boolean isPhysicallyPossibleHelper(int parentCurrent) {
+    /* TEMPLATE:
+    PARAMETERS:
+    ... parentCurrent ...   -- int
+     */
     return true;
   }
   
@@ -67,6 +87,18 @@ class Tip implements ILightningBolt {
   public ILightningBolt combine(int leftLength, int rightLength, int leftCapacity,
                                 int rightCapacity, double leftTheta, double rightTheta,
                                 ILightningBolt otherBolt) {
+    /* TEMPLATE:
+    PARAMETERS:
+    ... leftLength ...                          -- int
+    ... rightLength ...                         -- int
+    ... leftCapacity ...                        -- int
+    ... rightCapacity ...                       -- int
+    ... leftTheta ...                           -- double
+    ... rightTheta ...                          -- double
+    ... otherBolt ...                           -- ILightningBolt
+    METHODS ON PARAMETERS:
+    ... otherBolt.offsetAll(double theta) ...   -- ILightningBolt
+     */
     return new Fork(leftLength, rightLength, leftCapacity, rightCapacity, leftTheta, rightTheta,
       this.offsetAll(leftTheta - 90),
       otherBolt.offsetAll(rightTheta - 90));
@@ -74,6 +106,10 @@ class Tip implements ILightningBolt {
   
   // Returns this, since there are no angles to offset.
   public ILightningBolt offsetAll(double theta) {
+    /* TEMPLATE:
+    PARAMETERS:
+    ... theta ...   -- double
+     */
     return this;
   }
   
@@ -111,6 +147,34 @@ class Segment implements ILightningBolt {
     this.bolt = bolt;
   }
   
+  /* TEMPLATE:
+  FIELDS:
+  ... this.length ...                                              -- int
+  ... this.current ...                                             -- int
+  ... this.theta ...                                               -- double
+  ... this.bolt ...                                                -- ILightningBolt
+  METHODS:
+  ... draw() ...                                                   -- WorldImage
+  ... IsPhysicallyPossible() ...                                   -- boolean
+  ... isPhysicallyPossibleHelper(int parentCurrent) ...            -- boolean
+  ... combine(int leftLength, int rightLength,
+              int leftCapacity, int rightCapacity,
+              double leftTheta, double rightTheta,
+              ILightningBolt otherBolt) ...                        -- ILightningBolt
+  ... offsetAll(double theta) ...                                  -- ILightningBolt
+  ... getWidth() ...                                               -- double
+  ... getRightmostX() ...                                          -- double
+  ... getLeftmostX() ...                                           -- double
+  METHODS ON FIELDS:
+  ... this.bolt.draw() ...                                         -- WorldImage
+  ... this.bolt.isPhysicallyPossibleHelper(int parentCurrent)...   -- boolean
+  ... this.bolt.offsetAll(double theta)...                         -- ILightningBolt
+  ... this.bolt.getRightmostX()...                                 -- double
+  ... this.bolt.getLeftmostX()...                                  -- double
+   */
+  
+  // Draws this Segment as a red rectangle using the javalib library, angled this.theta degrees
+  // from the horizontal, with this.bolt rendered at the tip and the pinhole placed at the base.
   public WorldImage draw() {
     WorldImage segRect = new RectangleImage(this.length, 5, OutlineMode.SOLID, Color.RED)
                            .movePinholeTo(new Posn(-this.length / 2, 0));
@@ -118,7 +182,7 @@ class Segment implements ILightningBolt {
     // Rotate rest by -theta so that the final rotation by theta returns it to its original
     // position. This could be avoided by rotating segRectPinholed earlier, but then we would
     // need to use trig to reposition the pinhole.
-    WorldImage rest = new RotateImage(this.bolt.draw(), -theta);
+    WorldImage rest = new RotateImage(this.bolt.draw(), -this.theta);
     
     // Overlay rest on top of segRect
     WorldImage overlay = new OverlayOffsetAlign(AlignModeX.PINHOLE, AlignModeY.PINHOLE,
@@ -127,7 +191,7 @@ class Segment implements ILightningBolt {
     // Move the pinhole to the base of the segment
     WorldImage overlayPinholed = overlay.movePinhole(this.length, 0);
     // Rotate the segment and return
-    return new RotateImage(overlayPinholed, theta);
+    return new RotateImage(overlayPinholed, this.theta);
   }
   
   // Returns true iff this Segment is physically possible; i.e., its child has a current less than
@@ -140,6 +204,10 @@ class Segment implements ILightningBolt {
   // this to be true, its current must be less than parentCurrent, and its child must be physically
   // possible
   public boolean isPhysicallyPossibleHelper(int parentCurrent) {
+    /* TEMPLATE:
+    PARAMETERS:
+    ... parentCurrent ...   -- int
+     */
     return this.current <= parentCurrent && this.bolt.isPhysicallyPossibleHelper(this.current);
   }
   
@@ -149,6 +217,18 @@ class Segment implements ILightningBolt {
   public ILightningBolt combine(int leftLength, int rightLength, int leftCapacity,
                                 int rightCapacity, double leftTheta, double rightTheta,
                                 ILightningBolt otherBolt) {
+    /* TEMPLATE:
+    PARAMETERS:
+    ... leftLength ...                          -- int
+    ... rightLength ...                         -- int
+    ... leftCapacity ...                        -- int
+    ... rightCapacity ...                       -- int
+    ... leftTheta ...                           -- double
+    ... rightTheta ...                          -- double
+    ... otherBolt ...                           -- ILightningBolt
+    METHODS ON PARAMETERS:
+    ... otherBolt.offsetAll(double theta) ...   -- ILightningBolt
+     */
     return new Fork(leftLength, rightLength, leftCapacity, rightCapacity, leftTheta, rightTheta,
       this.offsetAll(leftTheta - 90),
       otherBolt.offsetAll(rightTheta - 90));
@@ -156,6 +236,9 @@ class Segment implements ILightningBolt {
   
   // Returns a new Segment whose theta, and all angles of children, are offset by the given theta.
   public ILightningBolt offsetAll(double theta) {
+    /* TEMPLATE:
+    ... theta ...   -- double
+     */
     return new Segment(this.length, this.current, this.theta + theta,
       this.bolt.offsetAll(theta));
   }
@@ -211,7 +294,44 @@ class Fork implements ILightningBolt {
     this.left = left;
     this.right = right;
   }
+  /* TEMPLATE:
+  FIELDS:
+  ... this.leftLength ...                                          -- int
+  ... this.rightLength ...                                         -- int
+  ... this.leftCurrent ...                                         -- int
+  ... this.rightCurrent ...                                        -- int
+  ... this.leftTheta ...                                           -- double
+  ... this.rightTheta ...                                          -- double
+  ... this.left ...                                                -- ILightningBolt
+  ... this.right ...                                               -- ILightningBolt
+  METHODS:
+  ... draw() ...                                                   -- WorldImage
+  ... isPhysicallyPossible() ...                                   -- boolean
+  ... isPhysicallyPossibleHelper(int parentCurrent) ...            -- boolean
+  ... combine(int leftLength, int rightLength,
+              int leftCapacity, int rightCapacity,
+              double leftTheta, double rightTheta,
+              ILightningBolt otherBolt) ...                        -- ILightningBolt
+  ... offsetAll(double theta) ...                                  -- ILightningBolt
+  ... getWidth() ...                                               -- double
+  ... getRightmostX() ...                                          -- double
+  ... getLeftmostX() ...                                           -- double
+  METHODS ON FIELDS:
+  ... this.left.draw() ...                                         -- WorldImage
+  ... this.right.draw() ...                                        -- WorldImage
+  ... this.left.isPhysicallyPossibleHelper(int parentCurrent) ...  -- boolean
+  ... this.right.isPhysicallyPossibleHelper(int parentCurrent) ... -- boolean
+  ... this.left.offsetAll(double theta) ...                        -- ILightningBolt
+  ... this.right.offsetAll(double theta) ...                       -- ILightningBolt
+  ... this.left.getRightmostX() ...                                -- double
+  ... this.right.getRightmostX() ...                               -- double
+  ... this.left.getLeftmostX() ...                                 -- double
+  ... this.right.getLeftmostX() ...                                -- double
+   */
   
+  // Draws this Fork as a red rectangle using the javalib library, with each branch angled
+  // according to the corresponding theta, the corresponding bolts rendered at their tips and the
+  // pinhole placed at the shared base.
   public WorldImage draw() {
     WorldImage leftRect = new RectangleImage(this.leftLength, 5, OutlineMode.SOLID, Color.RED)
                                .movePinholeTo(new Posn(-this.leftLength / 2, 0));
@@ -220,8 +340,8 @@ class Fork implements ILightningBolt {
     
     // Rotate remaining parts of the branch by -theta so that the final rotation by theta returns it
     // to its original position.
-    WorldImage leftRest = new RotateImage(this.left.draw(), -leftTheta);
-    WorldImage rightRest = new RotateImage(this.right.draw(), -rightTheta);
+    WorldImage leftRest = new RotateImage(this.left.draw(), -this.leftTheta);
+    WorldImage rightRest = new RotateImage(this.right.draw(), -this.rightTheta);
     
     // Overlay the remaining parts on top of the rects
     WorldImage leftOverlay = new OverlayOffsetAlign(AlignModeX.PINHOLE, AlignModeY.PINHOLE,
@@ -235,8 +355,8 @@ class Fork implements ILightningBolt {
     
     // Combine the left and right branches and return
     return new OverlayOffsetAlign(AlignModeX.PINHOLE, AlignModeY.PINHOLE,
-      new RotateImage(lOverlayPinholed, leftTheta), 0, 0,
-      new RotateImage(rOverlayPinholed, rightTheta));
+      new RotateImage(lOverlayPinholed, this.leftTheta), 0, 0,
+      new RotateImage(rOverlayPinholed, this.rightTheta));
   }
   
   // Returns true iff this Fork is physically possible; i.e., each the children of each of its
@@ -251,6 +371,10 @@ class Fork implements ILightningBolt {
   // to be true, the sum of the currents of its branches must be less than parentCurrent, and the
   // children of its branches must be physically possible.
   public boolean isPhysicallyPossibleHelper(int parentCurrent) {
+    /* TEMPLATE:
+    PARAMETERS:
+    ... parentCurrent ...   -- int
+     */
     return this.leftCurrent + this.rightCurrent <= parentCurrent
       && this.left.isPhysicallyPossibleHelper(this.leftCurrent)
       && this.right.isPhysicallyPossibleHelper(this.rightCurrent);
@@ -262,6 +386,18 @@ class Fork implements ILightningBolt {
   public ILightningBolt combine(int leftLength, int rightLength, int leftCapacity,
                                 int rightCapacity, double leftTheta, double rightTheta,
                                 ILightningBolt otherBolt) {
+    /* TEMPLATE:
+    PARAMETERS:
+    ... leftLength ...                      -- int
+    ... rightLength ...                     -- int
+    ... leftCapacity ...                    -- int
+    ... rightCapacity ...                   -- int
+    ... leftTheta ...                       -- double
+    ... rightTheta ...                      -- double
+    ... otherBolt ...                       -- ILightningBolt
+    METHODS ON PARAMETERS:
+    ... otherBolt.offsetAll(double theta)   -- ILightningBolt
+     */
     return new Fork(leftLength, rightLength, leftCapacity, rightCapacity, leftTheta, rightTheta,
       this.offsetAll(leftTheta - 90),
       otherBolt.offsetAll(rightTheta - 90));
@@ -270,6 +406,10 @@ class Fork implements ILightningBolt {
   // Returns a new Fork whose left and right theta, and all angles of children, are offset by
   // the given theta.
   public ILightningBolt offsetAll(double theta) {
+    /* TEMPLATE:
+    PARAMETERS:
+    ... theta ...   -- double
+     */
     return new Fork(this.leftLength, this.rightLength, this.leftCurrent, this.rightCurrent,
       this.leftTheta + theta, this.rightTheta + theta,
       this.left.offsetAll(theta), this.right.offsetAll(theta));
@@ -288,9 +428,9 @@ class Fork implements ILightningBolt {
   // - the tip of the right branch of the Fork, plus the rightmost point after that branch
   public double getRightmostX() {
     double lRightmost =
-      this.leftLength * Math.cos(Math.toRadians(this.leftTheta)) + this.left.getRightmostX();
+        this.leftLength * Math.cos(Math.toRadians(this.leftTheta)) + this.left.getRightmostX();
     double rRightmost =
-      this.rightLength * Math.cos(Math.toRadians(this.rightTheta)) + this.right.getRightmostX();
+        this.rightLength * Math.cos(Math.toRadians(this.rightTheta)) + this.right.getRightmostX();
     return Math.max(0, Math.max(lRightmost, rRightmost));
   }
   
@@ -301,9 +441,9 @@ class Fork implements ILightningBolt {
   // - the tip of the right branch of the Fork, plus the leftmost point after that branch
   public double getLeftmostX() {
     double lLeftmost =
-      this.leftLength * Math.cos(Math.toRadians(this.leftTheta)) + this.left.getLeftmostX();
+        this.leftLength * Math.cos(Math.toRadians(this.leftTheta)) + this.left.getLeftmostX();
     double rLeftmost =
-      this.rightLength * Math.cos(Math.toRadians(this.rightTheta)) + this.right.getLeftmostX();
+        this.rightLength * Math.cos(Math.toRadians(this.rightTheta)) + this.right.getLeftmostX();
     return Math.min(0, Math.min(lLeftmost, rLeftmost));
   }
 }
@@ -352,6 +492,19 @@ class ExamplesLightning {
     return t.checkExpect(fork1.draw(), expected);
   }
   
+  boolean testDrawSegment(Tester t) {
+    WorldImage segRect = new RectangleImage(40, 5, OutlineMode.SOLID, Color.RED)
+                           .movePinholeTo(new Posn(-40 / 2, 0));
+    WorldImage rest = new RotateImage(fork1.draw(), -130);
+    WorldImage overlay = new OverlayOffsetAlign(AlignModeX.PINHOLE, AlignModeY.PINHOLE,
+        rest, 0, 0, segRect);
+    WorldImage overlayPinholed = overlay.movePinhole(40, 0);
+    
+    WorldImage expected = new RotateImage(overlayPinholed, 130);
+    
+    return t.checkExpect(seg1.draw(), expected);
+  }
+  
   boolean testIsPhysicallyPossible1(Tester t) {
     return t.checkExpect(seg1.isPhysicallyPossible(), false);
   }
@@ -366,6 +519,19 @@ class ExamplesLightning {
   
   boolean testIsPhysicallyPossible4(Tester t) {
     return t.checkExpect(seg3.isPhysicallyPossible(), false);
+  }
+  
+  boolean testIsPhysicallyPossibleHelper(Tester t) {
+    return t.checkExpect(tip.isPhysicallyPossibleHelper(0), true)
+        && t.checkExpect(seg1.isPhysicallyPossibleHelper(20), false)
+        && t.checkExpect(forked.isPhysicallyPossibleHelper(70), true);
+  }
+  
+  boolean testOffsetAll(Tester t) {
+    return t.checkExpect(tip.offsetAll(30), tip)
+        && t.checkExpect(fork1.offsetAll(30),
+            new Fork(33, 30, 10, 25,165, 70, tip, tip))
+        && t.checkExpect(seg1.offsetAll(30), new Segment(40, 20, 160, fork1.offsetAll(30)));
   }
   
   boolean testCombine1(Tester t) {
@@ -444,6 +610,16 @@ class ExamplesLightning {
     return t.checkInexact(bolt.getWidth(),
       30 * Math.cos(Math.toRadians(40)) - 33 * Math.cos(Math.toRadians(135)), 0.001);
   }
+  
+  boolean testGetRightmostX(Tester t) {
+    return t.checkInexact(tip.getRightmostX(), 0.0, 0.001)
+        && t.checkInexact(fork1.getRightmostX(), 22.9813, 0.001)
+        && t.checkInexact(seg1.getRightmostX(), 0.0, 0.001);
+  }
+  
+  boolean testGetLeftmostX(Tester t) {
+    return t.checkInexact(tip.getLeftmostX(), 0.0, 0.001)
+        && t.checkInexact(fork1.getLeftmostX(), -23.334, 0.001)
+        && t.checkInexact(seg1.getLeftmostX(), -49.046, 0.001);
+  }
 }
-
-
