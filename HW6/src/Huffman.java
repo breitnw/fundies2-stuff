53b1d1cb66bbb593854cfbb9a5b304576e2c6b81
Nicholas Breitling <breitling.nw@gmail.com>
2024-02-28 13:58:21 -0500

    Tests for question 3

--- HW6/src/Huffman.java
+++ HW6/src/Huffman.java
@@ -21,7 +21,8 @@ class Huffman {
     
     // Zip chars and frequencies into a single list (NOTE: this also performs the validation that
     // the list lengths are equal)
-    ArrayList<IHuffmanTree> trees = new ListUtils().zip(chars, frequencies, new HuffmanTreeZipper());
+    ArrayList<IHuffmanTree> trees = new ListUtils().zip(
+        chars, frequencies, new HuffmanTreeZipper());
     
     // Condense all the IHuffmanTrees in the list into a single IHuffmanTree
     this.tree = new HuffmanTreeUtils().condense(trees);
@@ -137,13 +138,13 @@ class HTNode implements IHuffmanTree {
     left.mapMut(new ArrPrepend<>(false));
     right.mapMut(new ArrPrepend<>(true));
     
-    return left.or(right);
+    return left.orIfNone(right);
   }
   
   // Decodes a single character from the given Iterator of Booleans, or produces "?" if
   // the Iterator does not have enough elements left to decode a character.
-  // EFFECT: moves the cursor of this codeIter the number of elements necessary to decode a
-  // single character
+  // EFFECT: moves the cursor of codeIter the number of elements necessary to decode a single
+  // character
   public String decodeNext(Iterator<Boolean> codeIter) {
     if (codeIter.hasNext()) {
       boolean shouldGoRight = codeIter.next();
@@ -163,7 +164,7 @@ class HTNode implements IHuffmanTree {
 // Represents a value that may or may not exist
 interface Maybe<T> {
   // Returns this Maybe if it contains a value, otherwise returns that
-  Maybe<T> or(Maybe<T> that);
+  Maybe<T> orIfNone(Maybe<T> that);
   
   // If this Maybe has contents, mutates those contents according to f; otherwise, does nothing.
   void mapMut(Consumer<T> f);
@@ -181,7 +182,7 @@ class Some<T> implements Maybe<T> {
   }
   
   // Returns this Maybe if it contains a value, otherwise returns that
-  public Maybe<T> or(Maybe<T> that) {
+  public Maybe<T> orIfNone(Maybe<T> that) {
     return this;
   }
   
@@ -199,12 +200,14 @@ class Some<T> implements Maybe<T> {
 // Represents a value that does not exist
 class None<T> implements Maybe<T>  {
   // Returns this Maybe if it contains a value, otherwise returns that
-  public Maybe<T> or(Maybe<T> that) {
+  public Maybe<T> orIfNone(Maybe<T> that) {
     return that;
   }
   
   // If this Maybe has contents, mutates those contents according to f; otherwise, does nothing.
-  public void mapMut(Consumer<T> f) { }
+  public void mapMut(Consumer<T> f) {
+    // This method does not do anything, since None has no value to mutate
+  }
   
   // If this Maybe has contents, returns those contents; otherwise, throws ex.
   public T expect(RuntimeException ex) {
@@ -255,6 +258,7 @@ class HuffmanTreeZipper implements BiFunction<String, Integer, IHuffmanTree> {
 
 // UTILS ------------------------------------------------------------------------------------------
 
+// Represents a suite of utilities for working with generic lists.
 class ListUtils {
   // Sorts the provided ArrayList low-to-high via a stable insertion sort, provided a Comparator
   // cmp that compares any two elements of the ArrayList.
@@ -307,7 +311,8 @@ class HuffmanTreeUtils {
   // Condenses a list of IHuffmanTrees into a single IHuffmanTree, combining the trees with the
   // lowest total frequencies bottom-up.
   IHuffmanTree condense(ArrayList<IHuffmanTree> trees) {
-    ArrayList<IHuffmanTree> treesSorted = new ListUtils().sorted(trees, new HuffmanTreeComparator());
+    ArrayList<IHuffmanTree> treesSorted = new ListUtils().sorted(
+        trees, new HuffmanTreeComparator());
     
     int treesSize = treesSorted.size();
     // No matter the size of treesSorted, we will always need to make size-1 merges
@@ -405,6 +410,8 @@ class ExamplesHuffman {
       new ArrayList<>(List.of(1, 2, 3, 4))
   );
   
+  // Methods on Huffman ---------------------------------------------------------------------------
+  
   // When constructing a Huffman tree, the keys and frequencies should be sorted in the constructor
   void testConstructorSorting(Tester t) {
     t.checkExpect(hPairs, hPairsMixed);
@@ -447,7 +454,7 @@ class ExamplesHuffman {
   }
   
   
-  // an unterminated sequence should result in an "/" appended at the end of the decoded string
+  // an unterminated sequence should result in a "?" appended at the end of the decoded string
   void testUnterminatedDecode(Tester t) {
     t.checkExpect(
         hPairs.decode(new ArrayList<>(List.of(false))),
@@ -458,20 +465,6 @@ class ExamplesHuffman {
     t.checkExpect(
         hPairs.decode(new ArrayList<>(List.of(true, false, false, false, false, true, false))),
         "cab?");
-  }
-  
-  // We should be able to maintain a result by decoding then encoding, or encoding then decoding
-  // (unless the result ends in a ? or an error is thrown)
-  void testConsistentEncodeDecode(Tester t) {
-    t.checkExpect(
-        hPairs.decode(hPairs.encode("abacd")),
-        "abacd"
-    );
-    t.checkExpect(
-        hPairs.encode(
-            hPairs.decode(new ArrayList<>(List.of(true, false, false, false, false, true)))),
-        new ArrayList<>(List.of(true, false, false, false, false, true))
-    );
   }
 
   void testOneCharEncode(Tester t) {
@@ -506,12 +499,251 @@ class ExamplesHuffman {
     );
   }
   
-  void testHuffmanTreeFrequency(Tester t) {
+  // We should be able to maintain a result by decoding then encoding, or encoding then decoding
+  // (unless the result ends in a ? or an error is thrown)
+  void testConsistentEncodeDecode(Tester t) {
+    t.checkExpect(
+        hPairs.decode(hPairs.encode("abacd")),
+        "abacd"
+    );
+    t.checkExpect(
+        hPairs.encode(
+            hPairs.decode(new ArrayList<>(List.of(true, false, false, false, false, true)))),
+        new ArrayList<>(List.of(true, false, false, false, false, true))
+    );
+  }
+  
+  // Methods on IHuffmanTree ----------------------------------------------------------------------
+  
+  void testFrequency(Tester t) {
     IHuffmanTree leaf1 = new HTLeaf("a", 3);
     IHuffmanTree leaf2 = new HTLeaf("b", 2);
     IHuffmanTree node1 = new HTNode(leaf1, leaf2);
     t.checkExpect(leaf1.frequency(), 3);
     t.checkExpect(leaf2.frequency(), 2);
     t.checkExpect(node1.frequency(), 5);
+  }
+  
+  void testEncodeChar(Tester t) {
+    t.checkExpect(hTwo.tree.encodeChar("a"),
+        new Some<>(new ArrayList<>(List.of(false))));
+    t.checkExpect(hPairs.tree.encodeChar("c"),
+        new Some<>(new ArrayList<>(List.of(true, false))));
+    t.checkExpect(hPairs.tree.encodeChar("s"),
+        new None<>());
+    t.checkExpect(new HTLeaf("a", 2).encodeChar("a"),
+        new Some<>(new ArrayList<>()));
+    t.checkExpect(new HTLeaf("a", 2).encodeChar("b"),
+        new None<>());
+  }
+  
+  void testDecodeNext(Tester t) {
+    ArrayList<Boolean> code1 = new ArrayList<>(List.of(false, true, true));
+    ArrayList<Boolean> code2 = new ArrayList<>(List.of(true));
+    
+    // Iterator extends beyond the end of the tree, and one element is used
+    Iterator<Boolean> iter1 = code1.iterator();
+    t.checkExpect(hTwo.tree.decodeNext(iter1), "a");
+    // Check that the iterator is properly mutated
+    Iterator<Boolean> iter1Check = code1.iterator();
+    iter1Check.next();
+    t.checkExpect(iter1, iter1Check);
+    
+    // Iterator extends beyond the end of this tree, and two elements are used
+    Iterator<Boolean> iter2 = code1.iterator();
+    t.checkExpect(hPairs.tree.decodeNext(iter2), "b");
+    // Check that the iterator is properly mutated
+    Iterator<Boolean> iter2Check = code1.iterator();
+    iter2Check.next();
+    iter2Check.next();
+    t.checkExpect(iter2, iter2Check);
+    
+    // Iterator ends at the end of the tree
+    Iterator<Boolean> iter3 = code2.iterator();
+    t.checkExpect(hTwo.tree.decodeNext(iter3), "b");
+    // Check that the iterator is properly mutated
+    t.checkExpect(iter3.hasNext(), false);
+    
+    // Iterator ends before the end of the tree
+    Iterator<Boolean> iter4 = code2.iterator();
+    t.checkExpect(hPairs.tree.decodeNext(iter4), "?");
+    // Check that the iterator is properly mutated
+    t.checkExpect(iter4.hasNext(), false);
+    
+    // Tree is just a leaf
+    Iterator<Boolean> iter5 = code2.iterator();
+    t.checkExpect(new HTLeaf("a", 2).decodeNext(iter5), "a");
+    // Check that the iterator is properly mutated
+    t.checkExpect(iter5, code2.iterator());
+  }
+  
+  // Methods on Maybe -----------------------------------------------------------------------------
+  
+  void testOr(Tester t) {
+    t.checkExpect(new Some<>(1).orIfNone(new Some<>(2)), new Some<>(1));
+    t.checkExpect(new Some<>(1).orIfNone(new None<>()), new Some<>(1));
+    t.checkExpect(new None<>().orIfNone(new Some<>(2)), new Some<>(2));
+    t.checkExpect(new None<>().orIfNone(new None<>()), new None<>());
+  }
+  
+  void testMapMut(Tester t) {
+    ArrayList<Integer> ints = new ArrayList<>(List.of(1, 2));
+    Maybe<ArrayList<Integer>> someInts = new Some<>(ints);
+    Maybe<ArrayList<Integer>> noInts = new None<>();
+    
+    // A Some should be mutated according to the consumer
+    someInts.mapMut(new ArrPrepend<>(0));
+    t.checkExpect(someInts, new Some<>(new ArrayList<>(List.of(0, 1, 2))));
+    
+    // A None should not be changed
+    noInts.mapMut(new ArrPrepend<>(0));
+    t.checkExpect(noInts, new None<>());
+  }
+  
+  void testExpect(Tester t) {
+    t.checkExpect(new Some<>(1).expect(new RuntimeException("oops")), 1);
+  }
+  
+  void testExpectEx(Tester t) {
+    t.checkException(
+        new RuntimeException("oops"),
+        new None<>(),
+        "expect",
+        new RuntimeException("oops"));
+  }
+  
+  // Function objects -----------------------------------------------------------------------------
+  
+  void testArrPrepend(Tester t) {
+    ArrayList<Integer> l1 = new ArrayList<>(List.of(1, 2));
+    new ArrPrepend<>(0).accept(l1);
+    t.checkExpect(l1, new ArrayList<>(List.of(0, 1, 2)));
+    
+    ArrayList<String> l2 = new ArrayList<>(List.of("a", "b"));
+    new ArrPrepend<>("z").accept(l2);
+    t.checkExpect(l2, new ArrayList<>(List.of("z", "a", "b")));
+  }
+  
+  void testHuffmanTreeComparator(Tester t) {
+    IHuffmanTree lFreq1 = new HTLeaf("a", 1);
+    IHuffmanTree lFreq2 = new HTLeaf("b", 2);
+    IHuffmanTree lFreq3 = new HTLeaf("c", 3);
+    IHuffmanTree nFreq3 = new HTNode(lFreq2, lFreq1);
+    IHuffmanTree nFreq5 = new HTNode(lFreq2, lFreq3);
+    
+    t.checkExpect(new HuffmanTreeComparator().compare(lFreq1, lFreq1), 0);
+    t.checkExpect(new HuffmanTreeComparator().compare(lFreq1, lFreq2), -1);
+    t.checkExpect(new HuffmanTreeComparator().compare(lFreq2, lFreq1), 1);
+    t.checkExpect(new HuffmanTreeComparator().compare(lFreq1, lFreq3), -2);
+    
+    t.checkExpect(new HuffmanTreeComparator().compare(lFreq1, nFreq3), -2);
+    t.checkExpect(new HuffmanTreeComparator().compare(nFreq3, lFreq1), 2);
+    t.checkExpect(new HuffmanTreeComparator().compare(lFreq3, nFreq3), 0);
+    
+    t.checkExpect(new HuffmanTreeComparator().compare(nFreq3, nFreq5), -2);
+    t.checkExpect(new HuffmanTreeComparator().compare(nFreq5, nFreq3), 2);
+  }
+  
+  void testHuffmanTreeZipper(Tester t) {
+    t.checkExpect(new HuffmanTreeZipper().apply("a", 2), new HTLeaf("a", 2));
+    t.checkExpect(new HuffmanTreeZipper().apply("b", 5), new HTLeaf("b", 5));
+  }
+  
+  // Functions on ListUtils -----------------------------------------------------------------------
+  
+  void testSorted(Tester t) {
+    IHuffmanTree lFreq1 = new HTLeaf("a", 1);
+    IHuffmanTree lFreq2b = new HTLeaf("b", 2);
+    IHuffmanTree lFreq2c = new HTLeaf("c", 2);
+    IHuffmanTree lFreq3 = new HTLeaf("d", 3);
+    
+    ArrayList<IHuffmanTree> mt = new ArrayList<>();
+    ArrayList<IHuffmanTree> trees = new ArrayList<>(List.of(lFreq3, lFreq1, lFreq2b));
+    ArrayList<IHuffmanTree> treesDup = new ArrayList<>(List.of(lFreq3, lFreq1, lFreq2c, lFreq2b));
+    
+    t.checkExpect(new ListUtils().sorted(mt, new HuffmanTreeComparator()), mt);
+    t.checkExpect(new ListUtils().sorted(trees, new HuffmanTreeComparator()),
+        new ArrayList<>(List.of(lFreq1, lFreq2b, lFreq3)));
+    t.checkExpect(new ListUtils().sorted(treesDup, new HuffmanTreeComparator()),
+        new ArrayList<>(List.of(lFreq1, lFreq2c, lFreq2b, lFreq3)));
+  }
+  
+  void testInsert(Tester t) {
+    IHuffmanTree lFreq1 = new HTLeaf("a", 1);
+    IHuffmanTree lFreq2b = new HTLeaf("b", 2);
+    IHuffmanTree lFreq2c = new HTLeaf("c", 2);
+    IHuffmanTree lFreq3 = new HTLeaf("d", 3);
+    
+    ArrayList<IHuffmanTree> mt = new ArrayList<>();
+    ArrayList<IHuffmanTree> trees1 = new ArrayList<>(List.of(lFreq1, lFreq3));
+    ArrayList<IHuffmanTree> trees2 = new ArrayList<>(List.of(lFreq1, lFreq2b, lFreq3));
+    
+    new ListUtils().insert(mt, lFreq2c, new HuffmanTreeComparator());
+    t.checkExpect(mt, new ArrayList<>(List.of(lFreq2c)));
+    
+    new ListUtils().insert(trees1, lFreq2c, new HuffmanTreeComparator());
+    t.checkExpect(trees1, new ArrayList<>(List.of(lFreq1, lFreq2c, lFreq3)));
+    
+    new ListUtils().insert(trees2, lFreq2c, new HuffmanTreeComparator());
+    t.checkExpect(trees2, new ArrayList<>(List.of(lFreq1, lFreq2b, lFreq2c, lFreq3)));
+  }
+  
+  void testZip(Tester t) {
+    t.checkExpect(new ListUtils().zip(
+        new ArrayList<String>(),
+        new ArrayList<Integer>(),
+        new HuffmanTreeZipper()
+    ), new ArrayList<IHuffmanTree>());
+    
+    t.checkExpect(new ListUtils().zip(
+        new ArrayList<String>(List.of("a", "b", "c")),
+        new ArrayList<Integer>(List.of(1, 3, 2)),
+        new HuffmanTreeZipper()
+    ), new ArrayList<IHuffmanTree>(List.of(
+        new HTLeaf("a", 1),
+        new HTLeaf("b", 3),
+        new HTLeaf("c", 2)
+    )));
+  }
+  
+  // Functions on HuffmanTreeUtils ----------------------------------------------------------------
+  
+  void testCondense(Tester t) {
+    IHuffmanTree lFreq1 = new HTLeaf("a", 1);
+    IHuffmanTree lFreq2b = new HTLeaf("b", 2);
+    IHuffmanTree lFreq2c = new HTLeaf("c", 2);
+    IHuffmanTree lFreq3 = new HTLeaf("d", 3);
+    
+    IHuffmanTree nFreq3 = new HTNode(lFreq1, lFreq2b);
+    IHuffmanTree nFreq4 = new HTNode(lFreq2b, lFreq2c);
+    IHuffmanTree nFreq5 = new HTNode(lFreq2c, nFreq3);
+    IHuffmanTree nFreq6 = new HTNode(lFreq3, nFreq3);
+    
+    // Test behavior with only one leaf
+    t.checkExpect(new HuffmanTreeUtils().condense(
+        new ArrayList<>(List.of(lFreq1))
+    ), lFreq1);
+    
+    // Test behavior of merging two leaves
+    t.checkExpect(new HuffmanTreeUtils().condense(
+        new ArrayList<>(List.of(lFreq1, lFreq2b))
+    ), nFreq3);
+    
+    // Test behavior of merging two leaves of matching frequency (they should be kept in order)
+    t.checkExpect(new HuffmanTreeUtils().condense(
+        new ArrayList<>(List.of(lFreq2b, lFreq2c))
+    ), nFreq4);
+    
+    // Test behavior of inserting a node and then another node containing it (the leaf should
+    // come first since the node has a higher value for frequency)
+    t.checkExpect(new HuffmanTreeUtils().condense(
+        new ArrayList<>(List.of(lFreq1, lFreq2b, lFreq2c))
+    ), nFreq5);
+    
+    // Test behavior of merging a node and a leaf of matching frequency (the leaf should come first
+    // since the node is inserted after it)
+    t.checkExpect(new HuffmanTreeUtils().condense(
+        new ArrayList<>(List.of(lFreq1, lFreq2b, lFreq3))
+    ), nFreq6);
   }
 }
