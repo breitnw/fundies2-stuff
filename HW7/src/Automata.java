7f11c4149ccfb46a200d736ef1f26b3bb30937dd
Nicholas Breitling <breitling.nw@gmail.com>
2024-03-05 23:16:11 -0600

    bitwise automata rule representation

--- HW7/src/Automata.java
+++ HW7/src/Automata.java
@@ -1,106 +1,93 @@
 import tester.*; // The tester library
 
-import javalib.worldimages.*;   // images, like RectangleImage or OverlayImages
-import javalib.impworld.*;      // the abstract World class and the big-bang library for imperative worlds
-
-import java.awt.Color;
 // general colors (as triples of red,green,blue values)
 // and predefined colors (Red, Green, Yellow, Blue, Black, White)
-import java.util.ArrayList;
-import java.util.List;
 
 
 interface ICell {
-    // gets the state of this ICell
-    int getState();
-
-    // render this ICell as an image of a rectangle with this width and height
+  // gets the state of this ICell
+  int getState();
+  
+  // render this ICell as an image of a rectangle with this width and height
 //    WorldImage render(int width, int height);
-
-    // produces the child cell of this ICell with the given left and right neighbors
-    ICell childCell(ICell left, ICell right);
+  
+  // produces the child cell of this ICell with the given left and right neighbors
+  ICell childCell(ICell left, ICell right);
 }
 
 class InertCell implements ICell {
-    public int getState() {
-        return 0;
-    }
-
-    public ICell childCell(ICell left, ICell right) {
-        return new InertCell();
-    }
+  // gets the state of this ICell
+  public int getState() {
+    return 0;
+  }
+  
+  // produces the child cell of this ICell with the given left and right neighbors
+  public ICell childCell(ICell left, ICell right) {
+    return new InertCell();
+  }
 }
 
-class Utils {
-    // generates the index of a truth table as an ArrayList
-    public ArrayList<ArrayList<Integer>> generateMatrix(int dimensions, int depth) {
-        ArrayList<ArrayList<Integer>> mat = new ArrayList<>();
-
-        for (int i = (int)(Math.pow(depth, dimensions) - 1); i >= 0; i -= 1) {
-            ArrayList<Integer> row = new ArrayList<>();
-            for (int j = dimensions - 1; j >= 0; j -= 1) {
-                int val = (i / (int)Math.pow(depth, j)) % depth;
-                row.add(val);
-            }
-            mat.add(row);
-        }
-
-        return mat;
-    }
-}
-
-// hardcoded dimension 3 depth 2 cell
 abstract class ACell implements ICell {
-    static final ArrayList<ArrayList<Integer>> ruleMatrix = new Utils().generateMatrix(3, 2);
-
-
-
-    int state;
-    ArrayList<Integer> ruleVec;
-
-    ACell(int state, int ruleNum) {
-        this.state = state;
-        this.ruleVec = new ArrayList<>();
-
-        for (int i = 7; i >= 0; i -= 1) {
-            this.ruleVec.add((int) (ruleNum / (Math.pow(2, i))));
-        }
-    }
-
-
-    public int getState() {
-        return this.state;
-    }
-
-    public int nextState(int left, int right) {
-        if (this.ruleMatrix.get(0).size() != 3) {
-            throw new IllegalStateException("Invalid ruleMatrix");
-        }
-        ArrayList<Integer> vec = new ArrayList<>(List.of(left, this.getState(), right));
-        for (int i = 0; i < this.ruleMatrix.size(); i += 1) {
-            ArrayList<Integer> curVec = this.ruleMatrix.get(i);
-            boolean same = true;
-            for (int j = 0; j < curVec.size(); j += 1) {
-                same = same && curVec.get(j).equals(vec.get(j));
-            }
-            if (same) {
-                return this.ruleVec.get(i);
-            }
-        }
-        throw new RuntimeException("Invalid state found");
-    }
-
-
+  int rule;
+  int state;
+  
+  ACell(int state, int rule) {
+    this.state = state;
+    this.rule = rule;
+  }
+  
+  public int getState() {
+    return this.state;
+  }
+  
+  public int childState(ICell left, ICell right) {
+    /*
+    by masking ruleNum with 2 raised to the power of our state, we get a number that is only
+    positive when the rule's result for our state is positive.
+    
+    For example, consider rule 114, with the left and right cells on and this cell off.
+    - binaryState will be ((1*4)+(0*2)+(1*1)) = 5
+    - binaryStateMask will be 1 << 5 = 00100000
+    The binary representation of binaryStateMask will always have exactly one 1, lined up with
+    the corresponding output in our rule's binary representation. Thus, we can simply take the
+    bitwise AND:
+    
+      00100000
+    & 01110010
+      --------
+      00100000 > 0
+      
+    If the value is greater than 0, that means that our desired rule has an output of 1. So, this
+    cell should have a value of 1 for the next state.
+     */
+    int binaryState = left.getState() * 4 + this.getState() * 2 + right.getState();
+    int binaryStateMask = 1 << binaryState;
+    return Math.min(this.rule & binaryStateMask, 1);
+  }
 }
 
 class Rule60 extends ACell {
-    Rule60(int state) {
-        super(state, 60);
-    }
-    public ICell childCell(ICell left, ICell right) {
-        return new Rule60(this.nextState(left.getState(), right.getState()));
-    }
+  Rule60(int state) {
+    super(state, 60);
+  }
+  
+  public ICell childCell(ICell left, ICell right) {
+    return new Rule60(this.childState(left, right));
+  }
 }
 
-
-
+class ExamplesAutomata {
+  Rule60 s0 = new Rule60(0);
+  Rule60 s1 = new Rule60(1);
+  
+  void testNextState(Tester t) {
+    t.checkExpect(s0.childState(s0, s0), 0);
+    t.checkExpect(s0.childState(s0, s1), 0);
+    t.checkExpect(s1.childState(s0, s0), 1);
+    t.checkExpect(s1.childState(s0, s1), 1);
+    t.checkExpect(s0.childState(s1, s0), 1);
+    t.checkExpect(s0.childState(s1, s1), 1);
+    t.checkExpect(s1.childState(s1, s0), 0);
+    t.checkExpect(s1.childState(s1, s1), 0);
+  }
+}
